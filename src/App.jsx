378e9c17adbf54662ced8a2e4dda79bbb7ac9ec6
Emily Tsui <113730297+Emily-Tsui@users.noreply.github.com>
2024-09-30 16:46:23 -0700

    All files uploaded

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Flashcard from './components/Flashcard';
+import './App.css';
+import verbs from './verbs';
+
+function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === verbs.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const currentWord = verbs[currentIndex];
+
+  return (
+    <>
+
+      <div>
+        <h1>Spanish Verbs</h1>
+        <p>Please click on each card to reveal the definition of the verb.</p>
+      </div>
+
+      <Flashcard
+        word={currentWord.word}
+        definition={currentWord.definition}
+        type={currentWord.type} // Pass the type as a prop
+      />
+
+      <div className="controls">
+        <button onClick={handleNext}>Next Card</button>
+      </div>
+ 
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/verbs.js
@@ -0,0 +1,40 @@
+const verbs = [
+    {
+        word: "Hablar",
+        definition: "To speak",
+        type: "verb",
+      },
+      {
+        word: "Comer",
+        definition: "To eat",
+        type: "verb",
+      },
+      {
+        word: "Vivir",
+        definition: "To live",
+        type: "verb",
+      },
+      {
+        word: "Casa",
+        definition: "House",
+        type: "noun",
+      },
+      {
+        word: "Perro",
+        definition: "Dog",
+        type: "noun",
+      },
+      {
+        word: "Yo",
+        definition: "I",
+        type: "pronoun",
+      },
+      {
+        word: "Tú",
+        definition: "You",
+        type: "pronoun",
+      },
+      // Add more words as needed
+    ];
+    
+    export default verbs;
